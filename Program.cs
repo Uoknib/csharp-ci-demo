b6d7a337cc40f6236aa68a3afe5bf265be409ce7
Uoknib <138501709+Uoknib@users.noreply.github.com>
2026-05-26 09:22:29 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static int Add(int a, int b) => a + b;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"1 + 2 = {Add(1, 2)}");
+            Console.WriteLine("All good!");
+        }
+    }
+}
